1e66c0917b61c979c5cebb8d28b7c57436337c21
Sam Nelson <snelson@cs.uoregon.edu>
2017-11-09 21:52:59 -0800

    preload format/deck

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 import Tkinter as tk
+import os,json
+
+class Ragnarok:
+    def __init__(self, path):
+        self.path = path
+        self.records = []
+        self.load()
+    def load(self):
+        if not os.path.exists(self.path):
+            print "database doesn't exist, can't load"
+            return
+        with open(self.path) as f:
+            self.records = json.load(f)
+    def addRecord(self, rec):
+        self.records.append(rec)
+        self.save()
+    def save(self):
+        with open(self.path, "w") as f:
+            json.dump(self.records,f)
+    def getRecord(self,i):
+        # Get's the i'th newest record, or None if that index doesn't exist
+        if i >= len(self.records):
+            return None
+        return list(reversed(self.records))[i]
 
 class App:
-    def __init__(self, master):
+    def __init__(self, master, db):
+        self.db = db
         self.g1 = tk.StringVar()
         self.g2 = tk.StringVar()
         self.g3 = tk.StringVar()
@@ -16,12 +41,14 @@ class App:
         self.txt_format.pack()
 
         self.ent_format = tk.Entry(frame)
+        self.ent_format.insert(0,self.db.getRecord(0)['format'])
         self.ent_format.pack()
 
         self.txt_deck = tk.Label(frame, text="My Deck")
         self.txt_deck.pack()
 
         self.ent_deck = tk.Entry(frame)
+        self.ent_deck.insert(0,self.db.getRecord(0)['deck'])
         self.ent_deck.pack()
 
         self.txt_opp_deck = tk.Label(frame, text="Opp Deck")
@@ -73,16 +100,27 @@ class App:
                              text="Save Match", fg="red",
                              command=self.save)
         self.button.pack(side=tk.BOTTOM)
-
     def save(self):
+        record = {
+            "g1": self.g1.get(),
+            "g2": self.g2.get(),
+            "g3": self.g3.get(),
+            "format": self.ent_format.get(),
+            "deck": self.ent_deck.get(),
+            "opp_deck": self.ent_opp_deck.get(),
+            "notes": self.ent_notes.get()
+        }
         print("Saving now")
-        print("Game Results", self.g1.get(),self.g2.get(),self.g3.get())
-        print("Format", self.ent_format.get())
-        print("My Deck", self.ent_deck.get())
-        print("Opponents Deck", self.ent_opp_deck.get())
-        print("Match Notes", self.ent_notes.get())
+        print("Game Results", record['g1'], record['g2'], record['g3'])
+        print("Format", record['format'])
+        print("My Deck", record['deck'])
+        print("Opponents Deck", record['opp_deck'])
+        print("Match Notes", record['notes'])
+        self.db.addRecord(record)
+        #TODO blank out the inputs that are likely to change every match
 
+
+db = Ragnarok('records.db')
 root = tk.Tk()
-
-app = App(root)
+app = App(root, db)
 root.mainloop()
